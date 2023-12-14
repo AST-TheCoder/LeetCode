c8e01a8205c18ecf5ed31068fbc7eb47004ae971
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-12-14 21:12:32 +0600

    Time: 248 ms (20.24%), Space: 141.5 MB (20.64%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
-        vector<int> onesRow,zerosRow,onesCol,zerosCol;
+        vector<int> onesRow,onesCol;
         for(int i=0;i<grid.size();i++){
             int ones=0;
             for(int j=0;j<grid[i].size();j++) ones+=grid[i][j];
             onesRow.push_back(ones);
-            zerosRow.push_back(grid[i].size()-ones);
         }
         for(int j=0;j<grid[0].size();j++){
             int ones=0;
             for(int i=0;i<grid.size();i++) ones+=grid[i][j];
             onesCol.push_back(ones);
-            zerosCol.push_back(grid.size()-ones);
         }
         vector<vector<int>> ans;
         for(int i=0;i<grid.size();i++){
             vector<int> ret;
             for(int j=0;j<grid[i].size();j++){
-                ret.push_back(onesRow[i]+onesCol[j]-zerosRow[i]-zerosCol[j]);
+                ret.push_back(2*(onesRow[i]+onesCol[j])-grid[i].size()-grid.size());
             }
             ans.push_back(ret);
         }
