4188822aa24bb8a74a5dd83394ecac621b777cd7
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-12-15 10:42:25 +0600

    Time: 137 ms (39.34%), Space: 77.9 MB (34.54%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> cnt;
+        for(int i=0;i<nums.size();i++){
+            cnt[nums[i]]++;
+            if(i>k) cnt[nums[i-k-1]]--;
+            if(cnt[nums[i]]>1) return true;
+        }
+        return false;
+    }
+};
