9a99e0e0c81fd27813dfdcce428dc7d7820b6225
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2023-12-15 10:37:49 +0600

    Time: 94 ms (73.47%), Space: 70 MB (50.86%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> cnt;
+        for(int i=0;i<nums.size();i++){
+            cnt[nums[i]]++;
+            if(cnt[nums[i]]>1) return true;
+        }
+        return false;
+    }
+};
