60bb91fa9dea9fefa6273445c67a20d773893e1b
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2024-01-04 20:32:35 +0600

    Time: 201 ms (8.29%), Space: 85.6 MB (14.00%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -3,14 +3,14 @@ public:
     int cnt[1000001];
     int minOperations(vector<int>& nums) {
         memset(cnt,0,sizeof(cnt));
+        int c = 0, ans = 0;
         for(int i=0;i<nums.size();i++){
             cnt[nums[i]]++;
+            if(cnt[nums[i]]==1) c++;
+            else if(cnt[nums[i]] == 2) c--;
+            if(cnt[nums[i]]%3 == 1) ans++;
         }
-        int ans=0;
-        for(int i=1;i<=1000000;i++){
-            if(cnt[i]==1) return -1;
-            ans+=cnt[i]/3 + (cnt[i]%3!=0);
-        }
+        if(c) return -1;
         return ans;
     }
 };
